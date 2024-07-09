4b11bafe17f59664d23033cc79075e32b6b1303e
jackmagosa <jackmangosa@gmail.com>
2024-07-09 13:33:32 +0200

    fix: resolve TypeScript compilation and update React setup
    - Update tsconfig.json to allow file emission
    - Modify webpack config to use both babel-loader and ts-loader
    - Update index.tsx to use React 18 rendering method
    - Add App.tsx component
    - Ensure index.html has root div
    - Update package.json scripts
    
    Resolves the 'TypeScript emitted no output' error and sets up proper React 18 rendering."

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,42 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = (envVars) => {
-  const { env } = envVars;
-  const envConfig = require(`./webpack.${env}.js`);
-  const config = merge(commonConfig, envConfig);
-  return config;
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
