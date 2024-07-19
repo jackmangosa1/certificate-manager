3bdae6e3929a21abf1fe8cea5e7ade81c5109aa9
jackmagosa <jackmangosa@gmail.com>
2024-07-19 15:55:18 +0200

    task1-KAN-27 Add sidebar navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 import Container from './components/Container/Container';
+import Example1 from './pages/example-1/Example1';
+import Example2 from './pages/example-2/Example2';
+import Example3 from './pages/example-3/Example3';
+import Start from './pages/start/Start';
 
 export const App = () => {
   return (
-    <Container className="container">
-      <Navbar />
-    </Container>
+    <Router>
+      <Container className="container">
+        <Navbar />
+        <div className="main">
+          <Sidebar className="sidebar" />
+          <Routes>
+            <Route
+              path="/"
+              element={<Start />}
+            />
+            <Route
+              path="/machine-learning"
+              element={<Example1 />}
+            />
+            <Route
+              path="/machine-learning/example1"
+              element={<Example1 />}
+            />
+
+            <Route
+              path="/machine-learning/example2"
+              element={<Example2 />}
+            />
+            <Route
+              path="/machine-learning/example3"
+              element={<Example3 />}
+            />
+          </Routes>
+        </div>
+      </Container>
+    </Router>
   );
 };
