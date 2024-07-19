dce37e6149550439dd82faf1201543629576fb5a
jackmagosa <jackmangosa@gmail.com>
2024-07-19 22:14:29 +0200

    task1-KAN-27 Made sidebar responsive

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,52 @@ import Example1 from './pages/example-1/Example1';
 import Example2 from './pages/example-2/Example2';
 import Example3 from './pages/example-3/Example3';
 import Start from './pages/start/Start';
+import { useState } from 'react';
 
 export const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
       <Container className="container">
-        <Navbar className="navbar" />
+        <Navbar
+          className="navbar"
+          toggleSidebar={toggleSidebar}
+        />
+        <div
+          className={`overlay ${isSidebarOpen ? 'active' : ''}`}
+          onClick={toggleSidebar}
+        ></div>
         <div className="main">
-          <Sidebar className="sidebar" />
-          <Routes>
-            <Route
-              path="/"
-              element={<Start />}
-            />
-            <Route
-              path="/machine-learning"
-              element={<Example1 />}
-            />
-            <Route
-              path="/machine-learning/example1"
-              element={<Example1 />}
-            />
-
-            <Route
-              path="/machine-learning/example2"
-              element={<Example2 />}
-            />
-            <Route
-              path="/machine-learning/example3"
-              element={<Example3 />}
-            />
-          </Routes>
+          <Sidebar className={`sidebar ${isSidebarOpen ? 'open' : ''}`} />
+          <div className="content">
+            <Routes>
+              <Route
+                path="/"
+                element={<Start />}
+              />
+              <Route
+                path="/machine-learning"
+                element={<Example1 />}
+              />
+              <Route
+                path="/machine-learning/example1"
+                element={<Example1 />}
+              />
+              <Route
+                path="/machine-learning/example2"
+                element={<Example2 />}
+              />
+              <Route
+                path="/machine-learning/example3"
+                element={<Example3 />}
+              />
+            </Routes>
+          </div>
         </div>
       </Container>
     </Router>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,35 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ className }: { className: string }) => {
+const Navbar = ({
+  className,
+  toggleSidebar,
+}: {
+  className: string;
+  toggleSidebar: () => void;
+}) => {
   return (
     <div className={`navbar ${className}`}>
       <div className="left">
+        <svg
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+          className="menu-icon"
+          onClick={toggleSidebar}
+        >
+          <path
+            d="m0 0h24v24h-24z"
+            fill="none"
+          />
+          <path d="m3 18h18v-2h-18zm0-5h18v-2h-18zm0-7v2h18v-2z" />
+        </svg>
         <Link to="/">
           <h1>DCCS Tuzla</h1>
         </Link>
       </div>
-      <div className="right">
-        <p>test</p>
-      </div>
+      <div className="right"></div>
     </div>
   );
 };
