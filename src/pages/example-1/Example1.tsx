c368a459b408788a8c6cad103103ac01d7dcb2f9
jackmagosa <jackmangosa@gmail.com>
2024-07-23 13:03:38 +0200

    task2-KAN-43 Add table component

--- src/pages/example-1/Example1.tsx
+++ src/pages/example-1/Example1.tsx
@@ -1,42 +1,39 @@
 import React from 'react';
 import './Example1.css';
+import Table from '../../components/table/Table';
+import { Certificate } from '../../types/types';
 
 const Example2: React.FC = () => {
+  const headers = ['Supplier', 'Certificate type', 'Valid from', 'Valid to'];
+
+  const data: Certificate[] = [
+    {
+      supplier: 'DAIMLER AG, 1, Berlin',
+      certificateType: 'Permission of Printing',
+      validFrom: new Date('2017-08-21'),
+      validTo: new Date('2017-08-26'),
+    },
+    {
+      supplier: 'ANDEMIS GmbH, 1, Stuttgart',
+      certificateType: 'OHSAS 18001',
+      validFrom: new Date('2017-08-18'),
+      validTo: new Date('2017-08-24'),
+    },
+    {
+      supplier: 'ANDEMIS GmbH, 1, Stuttgart',
+      certificateType: 'Permission of Printing',
+      validFrom: new Date('2017-10-04'),
+      validTo: new Date('2017-10-10'),
+    },
+  ];
+
   return (
     <div className="content">
       <h1 className="title">Example 1</h1>
-      <div className="table-wrapper">
-        <table>
-          <thead>
-            <tr>
-              <th>Supplier</th>
-              <th>Certificate type</th>
-              <th>Valid from</th>
-              <th>Valid to</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>DAIMLER AG, 1, Berlin</td>
-              <td>Permission of Printing</td>
-              <td>21.08.2017</td>
-              <td>26.08.2017</td>
-            </tr>
-            <tr>
-              <td>ANDEMIS GmbH, 1, Stuttgart</td>
-              <td>OHSAS 18001</td>
-              <td>18.08.2017</td>
-              <td>24.08.2017</td>
-            </tr>
-            <tr>
-              <td>ANDEMIS GmbH, 1, Stuttgart</td>
-              <td>Permission of Printing</td>
-              <td>04.10.2017</td>
-              <td>10.10.2017</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <Table
+        headers={headers}
+        data={data}
+      />
     </div>
   );
 };
