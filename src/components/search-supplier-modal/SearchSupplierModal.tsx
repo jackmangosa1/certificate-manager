2a239ce0d3fca7f93c97ae25555e8aa4aa48debe
jackmagosa <jackmangosa@gmail.com>
2024-07-25 09:10:09 +0200

    task6-KAN-63 Implemented supplier lookup functionality

--- src/components/search-supplier-modal/SearchSupplierModal.tsx
+++ src/components/search-supplier-modal/SearchSupplierModal.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSuppliers } from '../../hooks/useSuppliers';
+import { initializeDatabase } from '../../utils/indexed-db/indexedDb';
 import './SearchSupplierModal.css';
 import { Supplier } from '@/types/types';
 
 type SupplierSearchModalProps = {
   isOpen: boolean;
   onClose: () => void;
-  onSelect: (supplier: string) => void;
+  onSelect: (supplier: Supplier) => void;
 };
 
 const SupplierSearchModal: React.FC<SupplierSearchModalProps> = ({
@@ -14,23 +15,29 @@ const SupplierSearchModal: React.FC<SupplierSearchModalProps> = ({
   onClose,
   onSelect,
 }) => {
-  const { searchSuppliers, initializeAndFetchSuppliers } = useSuppliers();
+  const { searchSuppliers } = useSuppliers();
   const [searchResults, setSearchResults] = useState<Supplier[]>([]);
   const [searchCriteria, setSearchCriteria] = useState({
     name: '',
     index: '',
     city: '',
   });
+  const [initialResults, setInitialResults] = useState<Supplier[]>([]);
+  const [selectedSupplier, setSelectedSupplier] = useState<Supplier | null>(
+    null,
+  );
 
   useEffect(() => {
     if (isOpen) {
-      const fetchInitialData = async () => {
-        const initialSuppliers = await initializeAndFetchSuppliers();
-        setSearchResults(initialSuppliers);
+      const initializeAndFetch = async () => {
+        await initializeDatabase();
+        const results = await searchSuppliers({});
+        setInitialResults(results);
+        setSearchResults(results);
       };
-      fetchInitialData();
+      initializeAndFetch();
     }
-  }, [isOpen, initializeAndFetchSuppliers]);
+  }, [isOpen]);
 
   if (!isOpen) return null;
 
@@ -43,23 +50,29 @@ const SupplierSearchModal: React.FC<SupplierSearchModalProps> = ({
   };
 
   const handleSearch = async () => {
-    if (Object.values(searchCriteria).some((value) => value !== '')) {
-      const results = await searchSuppliers(searchCriteria);
-      setSearchResults(results);
-    } else {
-      const allSuppliers = await initializeAndFetchSuppliers();
-      setSearchResults(allSuppliers);
+    const results = await searchSuppliers(searchCriteria);
+    setSearchResults(results);
+  };
+
+  const handleReset = () => {
+    setSearchCriteria({ name: '', index: '', city: '' });
+    setSearchResults(initialResults);
+  };
+
+  const handleRadioChange = (supplier: Supplier) => {
+    setSelectedSupplier(supplier);
+  };
+
+  const handleSelect = () => {
+    if (selectedSupplier) {
+      onSelect(selectedSupplier);
+      onClose();
     }
   };
 
-  const handleReset = async () => {
-    setSearchCriteria({ name: '', index: '', city: '' });
-    const allSuppliers = await initializeAndFetchSuppliers();
-    setSearchResults(allSuppliers);
-  };
-
-  const handleSelect = (supplier: Supplier) => {
-    onSelect(supplier.name);
+  const handleCloseModal = () => {
+    handleReset();
+    setSelectedSupplier(null);
     onClose();
   };
 
@@ -70,7 +83,7 @@ const SupplierSearchModal: React.FC<SupplierSearchModalProps> = ({
           <h2>Search for suppliers</h2>
           <button
             className="close-button"
-            onClick={onClose}
+            onClick={handleCloseModal}
           >
             ×
           </button>
@@ -146,14 +159,15 @@ const SupplierSearchModal: React.FC<SupplierSearchModalProps> = ({
                   {searchResults.map((supplier) => (
                     <tr key={supplier.id}>
                       <td>
-                        <svg
-                          viewBox="0 0 512 512"
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="gear-icon-supplier"
-                          onClick={() => handleSelect(supplier)}
-                        >
-                          <path d="m487.4 315.7-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3l-42.6 24.6c-17.9-15.4-38.5-27.3-60.8-35.1v-49.1c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7v49.2c-22.2 7.9-42.8 19.8-60.8 35.1l-42.5-24.6c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14l42.6 24.6c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zm-231.4 20.3c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z" />
-                        </svg>
+                        <td>
+                          <input
+                            type="radio"
+                            name="selectedSupplier"
+                            value={supplier.id}
+                            checked={selectedSupplier?.id === supplier.id}
+                            onChange={() => handleRadioChange(supplier)}
+                          />
+                        </td>
                       </td>
                       <td>{supplier.name}</td>
                       <td>{supplier.index}</td>
@@ -165,10 +179,16 @@ const SupplierSearchModal: React.FC<SupplierSearchModalProps> = ({
             </div>
           </div>
           <div className="modal-footer">
-            <button className="select-button">Select</button>
+            <button
+              className="select-button"
+              onClick={handleSelect}
+              disabled={!selectedSupplier}
+            >
+              Select
+            </button>
             <button
               className="cancel-button"
-              onClick={onClose}
+              onClick={handleCloseModal}
             >
               Cancel
             </button>
