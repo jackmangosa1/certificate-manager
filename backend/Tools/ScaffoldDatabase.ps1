7c10c242de3bd29ca733e807ef359fc47712988e
jackmagosa <jackmangosa@gmail.com>
2024-09-23 17:34:01 +0200

    Add Script and Tools folders`

---
+++ backend/Tools/ScaffoldDatabase.ps1
@@ -0,0 +1,9 @@
+﻿
+$connectionString = "Data Source=DESKTOP-0MMHJ8C\\SQLEXPRESS;Initial Catalog=CertificateManagerDB;Integrated Security=True;Trust Server Certificate=True"
+$outputDir = "./Entities"
+
+dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer `
+  -o $outputDir --context-dir ./Data --force
+
+dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer ` -o $outputDir --context-dir ./Data
+
