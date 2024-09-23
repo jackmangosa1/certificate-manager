c241068c291e3c38b201f7924614eebdf4c455b0
jackmagosa <jackmangosa@gmail.com>
2024-09-23 15:35:01 +0200

    Scaffold data models

---
+++ backend/Models/Certificate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class Certificate
+{
+    public int CertificateId { get; set; }
+
+    public int SupplierId { get; set; }
+
+    public int CertificateTypeId { get; set; }
+
+    public DateOnly ValidFrom { get; set; }
+
+    public DateOnly ValidTo { get; set; }
+
+    public string PdfDocumentUrl { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public byte[]? RowVersion { get; set; }
+
+    public virtual ICollection<CertificateAssignment> CertificateAssignments { get; set; } = new List<CertificateAssignment>();
+
+    public virtual CertificateType CertificateType { get; set; } = null!;
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public virtual Supplier Supplier { get; set; } = null!;
+}
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b77ebb2f4ec1ab70fc4cf8ee9ff43c0ebbd54f0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c7fa9988008b41aff3fdf35020e6c96921e284")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
