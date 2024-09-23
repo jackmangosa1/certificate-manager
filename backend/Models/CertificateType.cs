7530674e3987adcf1d10a78b9ed7584d1aefd540
jackmagosa <jackmangosa@gmail.com>
2024-09-23 14:23:07 +0200

    Fix the monorepo setup

---
+++ backend/Models/CertificateType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class CertificateType
+{
+    public int CertificateTypeId { get; set; }
+
+    public string CertificateTypeName { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public byte[]? RowVersion { get; set; }
+
+    public virtual ICollection<Certificate> Certificates { get; set; } = new List<Certificate>();
+}
