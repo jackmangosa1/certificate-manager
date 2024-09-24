4ceb2dca0e0d008bedff4d6d2249a054bbd1eb2e
jackmagosa <jackmangosa@gmail.com>
2024-09-24 11:48:59 +0200

    Reomve the connection string from the scaffold DB script

--- backend/Tools/ScaffoldDatabase.ps1
+++ backend/Tools/ScaffoldDatabase.ps1
@@ -1,9 +1,7 @@
-﻿
-$connectionString = "Data Source=DESKTOP-0MMHJ8C\\SQLEXPRESS;Initial Catalog=CertificateManagerDB;Integrated Security=True;Trust Server Certificate=True"
-$outputDir = "./Entities"
+﻿$outputDir = "./Entities"
 
-dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer `
-  -o $outputDir --context-dir ./Data --force
+dotnet ef dbcontext scaffold Name=ConnectionStrings:DefaultConnectionString Microsoft.EntityFrameworkCore.SqlServer `
+  -o $outputDir --context-dir ./Data
 
-dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer ` -o $outputDir --context-dir ./Data
+
 
